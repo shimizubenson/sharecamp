4958b6373d0212a9d5dc1426d4adb8d275a1bc40
shimizubenson <benson69.smz@gmail.com>
2022-09-08 18:51:53 +0900

    active_hashの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,22 +1,28 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, except: [:index, :show]
+  before_action :authenticate_user!, except: [:show, :index ]
+
 
   def index
+    @items = Item.order("created_at DESC")
   end
 
   def new
     @item = Item.new
   end
 
-  private
-
-  def items_params
-    params.require(:item).permit(, :image).merge(user_id: current_user.id)
-  end
-
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
     end
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :price, :description, :category_id, :day_to_ship_id, :image).merge(user_id: current_user.id)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
+  validates :name, :description,:image,             presence: true
+  validates :category_id, numericality:    { other_than: 1,message: "を入力してください" } 
+  validates :day_to_ship_id, numericality: { other_than: 1,message: "を入力してください" } 
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
+  belongs_to :category
+  belongs_to :day_to_ship
   has_one_attached :image
 end
