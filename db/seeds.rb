f4597af02e571c20b6e7c26b0c88981e1614f9d8
shimizubenson <benson69.smz@gmail.com>
2022-09-13 23:32:43 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
-User.create!(
-  nickname: 'ENV['SECRET_NICKNAME']',
+@user = User.create!(
+  nickname: ENV['SECRET_NICKNAME'],
   first_name: '山田',
   last_name: '太郎',
   first_name_kana: 'ヤマダ',
   last_name_kana: 'タロウ',
-  email: 'ENV['SECRET_EMAIL']',
-  password: 'ENV['SECRET_PASSWORD']',
-  password_confirmation: 'ENV['SECRET_PASSWORD_CONFIRMATION']',
+  email: ENV['SECRET_EMAIL'],
+  password: ENV['SECRET_PASSWORD'],
+  password_confirmation: ENV['SECRET_PASSWORD_CONFIRMATION'],
   birthday: '1986-03-05',
   admin: true
 )
