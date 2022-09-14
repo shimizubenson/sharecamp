957625b42586c45953b12cb01c8970b7d5482c8f
shimizubenson <benson69.smz@gmail.com>
2022-09-14 16:50:57 +0900

    Update rental_days.rb

--- app/models/rental_days.rb
+++ app/models/rental_days.rb
@@ -1,9 +1,7 @@
 class RentalDays < ActiveHash::Base
   self.data = [
     { id: 1, name: '---' },
-    { id: 2, name: '3泊4日' },
-    { id: 3, name: '1週間' },
-    { id: 4, name: '月額利用' }
+    { id: 2, name: '1週間' }
   ]
   include ActiveHash::Associations
   has_many :items
